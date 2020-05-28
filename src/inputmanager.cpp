41ddd73257454a61c641a4b58ded81c276f35d09
ninoh-fox <ninoh-fox@debian>
2020-05-28 20:09:27 +0200

    reañadida input.conf desde sd externa
    se ha vuelto a habilitar la opcion de poner leer la configuracion personalida de input.cfg desde la sd externa, si quieres saber el mapeo de tu gamepad solo tiene que teclear jstest /dev/input/jsX (X=0 es el propio mapeo de la consola) en el terminal

--- src/inputmanager.cpp
+++ src/inputmanager.cpp
@@ -43,11 +43,15 @@ bool InputManager::init(GMenu2X *gmenu2x, Menu *menu) {
 
 	/* If a user-specified input.conf file exists, we load it;
 	 * otherwise, we load the default one. */
-	string input_file = GMENU2X_SYSTEM_DIR "/input.conf";
+	string input_file = gmenu2x->getHome() + "/input.conf";
 	DEBUG("Loading user-specific input.conf file: %s.\n", input_file.c_str());
 	if (!readConfFile(input_file)) {
+		input_file = GMENU2X_SYSTEM_DIR "/input.conf";
+		DEBUG("Loading system input.conf file: %s.\n", input_file.c_str());
+		if (!readConfFile(input_file)) {
 			ERROR("InputManager: failed to open config file\n");
 			return false;
+		}
 		}
 
 	return true;
