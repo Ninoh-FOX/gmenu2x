c6a9eaf964624882e0a8c6925a65fbf40415bf46
RafaVico <saboteur2@hotmail.com>
2020-05-31 16:03:18 +0200

    Función para calcular la carga media de la batería

--- src/battery.cpp
+++ src/battery.cpp
@@ -6,6 +6,9 @@
 #include <cstdio>
 #include <sstream>
 
+int lastChecking=SDL_GetTicks();
+int batt_average[10]={0,0,0,0,0,0,0,0,0,0};   // Array con los últimos 10 valores leídos de la batería
+int battavg_idx=0;                            // Índice al valor que toca actualizar en la próxima lectura
 
 /**
  * Reads the current battery state and returns a number representing its level
@@ -16,44 +19,69 @@
 // la función de lectura de batería se divide en 2, una para ver carga y otra para ver si está conectada por usb
 unsigned short getBatteryLevel()
 {
-	FILE *batteryHandle = NULL;
+  int battval=0;
 
+  // si han pasado más de 2 minutos sin leer datos, marca la lectura a 0 para actualizar todos los valores
+  if((SDL_GetTicks()-lastChecking)>120000)
+    batt_average[battavg_idx]=0;
+
+  // si la lectura está a 0 o han pasado al menos 5 segundos, hace una nueva lectura
+  if(batt_average[battavg_idx]==0 || (SDL_GetTicks()-lastChecking)>5000) {
+    lastChecking=SDL_GetTicks();
+
+    FILE *batteryHandle = NULL;
 #if defined(PLATFORM_A320) || defined(PLATFORM_GCW0) || defined(PLATFORM_NANONOTE)
     batteryHandle = fopen("/sys/class/power_supply/battery/voltage_now", "r");
 #endif
-	if (batteryHandle) {
-	#ifdef BATTERY_RG350
-	/* voltaje maximo de la RG es 4320000 */
-	#define MAX_VOLTAGE 4250000
-	#define MIN_VOLTAGE 3330000
-	/* voltaje maximo de la RG es 4385000 con el cable USB */
-	#define USB_VOLTAGE 65000
-	#elif BATTERY_PG2
-	/* voltaje maximo de la PG es 4270000 */
-	#define MAX_VOLTAGE 4200000
-	#define MIN_VOLTAGE 3330000
-	/* voltaje maximo de la PG es 4321000 con el cable USB*/
-	#define USB_VOLTAGE 121000
-	#endif
+    if (batteryHandle) {
+      #ifdef BATTERY_RG350
+      /* voltaje maximo de la RG es 4320000 */
+      #define MAX_VOLTAGE 4250000
+      #define MIN_VOLTAGE 3330000
+      /* voltaje maximo de la RG es 4385000 con el cable USB */
+      #define USB_VOLTAGE 65000
+      #elif BATTERY_PG2
+      /* voltaje maximo de la PG es 4270000 */
+      #define MAX_VOLTAGE 4200000
+      #define MIN_VOLTAGE 3330000
+      /* voltaje maximo de la PG es 4321000 con el cable USB*/
+      #define USB_VOLTAGE 121000
+      #endif
 
-        int battval = 0;
-        fscanf(batteryHandle, "%d", &battval);
-		if (isBatteryCharging()) {
-		battval=((battval - MIN_VOLTAGE) - USB_VOLTAGE) * 100 / (MAX_VOLTAGE - MIN_VOLTAGE);
-		}else{
-		battval=(battval - MIN_VOLTAGE) * 100 / (MAX_VOLTAGE - MIN_VOLTAGE);
-		}
-		fclose(batteryHandle);
-			
-		
-		if(battval>100)
-		battval=100;
-		else if(battval<0)
-		battval=0;
-		return battval;
+      fscanf(batteryHandle, "%d", &battval);
+      if (isBatteryCharging()) {
+        battval=((battval - MIN_VOLTAGE) - USB_VOLTAGE) * 100 / (MAX_VOLTAGE - MIN_VOLTAGE);
+      } else {
+        battval=(battval - MIN_VOLTAGE) * 100 / (MAX_VOLTAGE - MIN_VOLTAGE);
+      }
+      fclose(batteryHandle);
+
+      if(battval>100)
+        battval=100;
+      else if(battval<0)
+        battval=0;
+
+      // si el array está a 0, estamos leyendo por primera vez, actualizamos el array completo
+      if(batt_average[battavg_idx]==0) {
+        for(int f=0; f<10; f++)
+          batt_average[f]=battval;
+        return battval;
+      }
+
+      // actualizamos la casilla del array con el nuevo valor leído
+      batt_average[battavg_idx++]=battval;
+      if(battavg_idx>9)
+        battavg_idx=0;
     }
+  }
 
-    return 0;
+  // calculamos la media de los últimos 10 valores leídos
+  battval=0;
+  for(int f=0; f<10; f++)
+    battval=battval+batt_average[f];
+  battval=battval/10;
+
+  return battval;
 }
 
 unsigned short isBatteryCharging()
